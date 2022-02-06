59774683a527afb706ce6b82064e1b87ba6798b1
Daniel Williams <danielwoz@gmail.com>
2022-02-07 04:15:14 +0800

    Fix the order of init arguments for TeslaPowerwall data in setup.py

--- setup.py
+++ setup.py
@@ -67,8 +67,8 @@ def main():
           'locally: '))
 
   # Power Data.
-  powerwall = TeslaPowerwallData(config.tesla_username, config.local_timezone,
-                                 db)
+  powerwall = TeslaPowerwallData(config.tesla_username, db,
+                                 config.local_timezone)
   root.info("Setting up powerwall data database tables.")
   powerwall.setup()
   root.info("Backfilling powerwall data. (Depending on how long installation "
